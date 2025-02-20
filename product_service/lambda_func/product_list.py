b0232c7c76747360a6a8d4592daf30fb273d238c
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-02-20 07:50:16 +0100

    Backend Server and Functionality Deployed

---
+++ product_service/lambda_func/product_list.py
@@ -0,0 +1,14 @@
+from products_mock import products
+import json
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET'
+        },
+        'body': json.dumps(products)
+    }
