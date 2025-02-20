23734bc64a581f327bd01c3c6624ee31adf8cfc2
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-02-20 16:56:25 +0100

    Modified API and Lambda

--- product_service/product_service_stack.py
+++ product_service/product_service_stack.py
@@ -1,95 +1,18 @@
-from aws_cdk import (
-    Stack,
-    aws_lambda as _lambda,
-    aws_apigateway as apigw,
-    CfnOutput
-)
+from aws_cdk import Stack
 from constructs import Construct
+from product_service.lambda_stack import LambdaStack
+from product_service.api_gateway_stack import ApiGatewayStack
 
 class ProductServiceStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Create Lambda functions
-        list_products_lambda = _lambda.Function(
-            self, 'ListProductsFunction',
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            handler='product_list.handler',
-            code=_lambda.Code.from_asset('product_service/lambda_func')
-        )
+        # Create Lambda Stack
+        lambda_stack = LambdaStack(self, "ProductLambdaStack")
 
-        get_product_lambda = _lambda.Function(
-            self, 'GetProductByIdFunction',
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            handler='product_by_id.handler',
-            code=_lambda.Code.from_asset('product_service/lambda_func')
-        )
-
-        # Create API Gateway with CORS
-        api = apigw.RestApi(
-            self, 'ProductsApi',
-            rest_api_name='Products Service',
-            default_cors_preflight_options=apigw.CorsOptions(
-                allow_origins=apigw.Cors.ALL_ORIGINS,
-                allow_methods=apigw.Cors.ALL_METHODS,
-                allow_headers=[
-                    'Content-Type',
-                    'X-Amz-Date',
-                    'Authorization',
-                    'X-Api-Key',
-                    'X-Amz-Security-Token',
-                ],
-                # max_age=Duration.seconds(300)
-            )
-        )
-
-        # Create API resources and methods
-        products = api.root.add_resource('products')
-        products.add_method(
-            'GET',
-            apigw.LambdaIntegration(
-                list_products_lambda,
-                proxy=True,
-                integration_responses=[{
-                    'statusCode': '200',
-                    'responseParameters': {
-                        'method.response.header.Access-Control-Allow-Origin': "'*'"
-                    }
-                }]
-            ),
-            method_responses=[{
-                'statusCode': '200',
-                'responseParameters': {
-                    'method.response.header.Access-Control-Allow-Origin': True
-                }
-            }]
-        )
-
-        product = products.add_resource('{id}')
-        product.add_method(
-            'GET',
-            apigw.LambdaIntegration(
-                get_product_lambda,
-                proxy=True,
-                integration_responses=[{
-                    'statusCode': '200',
-                    'responseParameters': {
-                        'method.response.header.Access-Control-Allow-Origin': "'*'"
-                    }
-                }]
-            ),
-            method_responses=[{
-                'statusCode': '200',
-                'responseParameters': {
-                    'method.response.header.Access-Control-Allow-Origin': True
-                }
-            }]
-        )
-
-        # Output the API URL
-        CfnOutput(
-            self, "APIGatewayURL",
-            value=f"{api.url}products",
-            description="API Gateway endpoint URL"
+        # Create API Gateway Stack
+        api_gateway_stack = ApiGatewayStack(
+            self, 
+            "ProductApiGatewayStack",
+            lambda_stack=lambda_stack
         )
